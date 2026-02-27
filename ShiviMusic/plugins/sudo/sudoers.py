b28509f11a409a2836efd19efae626bd11a08473
Badnam465 <kritikumari8665@gmail.com>
2026-02-27 21:51:02 +0530

    Update alert message for non-sudo users

--- ShiviMusic/plugins/sudo/sudoers.py
+++ ShiviMusic/plugins/sudo/sudoers.py
@@ -71,7 +71,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝗥ᴀᴜsʜᴀɴ ᴋᴀ 𝗟ᴜɴᴅ 𝗟ᴇɢᴀ 😂 \n 𝗦ᴜᴅᴏʟɪsᴛ 𝗗ᴇᴋʜɴᴇ ᴀᴀʏᴀ ᴍᴄ 🤧🖕", show_alert=True)
+        return await callback_query.answer("𝘽ᴀᴅɴᴀᴍ 𝘿ᴇᴠ ᴋᴀ 𝗟ᴜɴᴅ 𝗟ᴇɢᴀ 😂 \n 𝗦ᴜᴅᴏʟɪsᴛ 𝗗ᴇᴋʜɴᴇ ᴀᴀʏᴀ ᴍᴄ 🤧🖕", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
