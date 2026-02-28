e73944374e65f02e7dd5ab8fe06640e5516e33fb
Badnam465 <kritikumari8665@gmail.com>
2026-02-28 21:49:05 +0530

    Update ping command and remove eco command

--- ShiviMusic/plugins/tools/ping.py
+++ ShiviMusic/plugins/tools/ping.py
@@ -1,33 +1,50 @@
-# ===========================================================
-# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 🚀
-# 
-# This source code is under MIT License 📜
-# ❌ Unauthorized forking, importing, or using this code
-#    without giving proper credit will result in legal action ⚠️
-# 
-# 📩 DM for permission : @TheSigmaCoder
-# ===========================================================
+# -----------------------------------------------
+# 🔸 VampireMusic Project
+# 🔹 Developed & Maintained by: Vampire Bots (https://github.com/TEAM-VAMPIRE-OP)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by TEAM-VAMPIRE-OP
+# -----------------------------------------------
 
+import random
 from datetime import datetime
+from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from config import *
 from ShiviMusic import app
-from ShiviMusic.core.call import Shivi
+from ShiviMusic.core.call import Vampire
 from ShiviMusic.utils import bot_sys_stats
 from ShiviMusic.utils.decorators.language import language
 from ShiviMusic.utils.inline import supp_markup
-from config import BANNED_USERS
-from pyrogram import Client, filters
-from ShiviMusic.misc import SUDOERS
+from config import BANNED_USERS, PING_IMG_URL
 
+Shivi_PIC = [
+    "https://files.catbox.moe/fh7vw7.jpg",
+    "https://files.catbox.moe/lckxh6.jpg",
+    "https://files.catbox.moe/smteo6.jpg",
+    "https://files.catbox.moe/7enu2i.jpg",
+    "https://files.catbox.moe/n6hkvd.jpg",
+    "https://files.catbox.moe/ej1p7t.jpg",
+    "https://files.catbox.moe/fh7vw7.jpg",
+    "https://files.catbox.moe/lckxh6.jpg",
+    "https://files.catbox.moe/smteo6.jpg",
+    "https://files.catbox.moe/7enu2i.jpg",
+    "https://files.catbox.moe/n6hkvd.jpg",
+    "https://files.catbox.moe/ej1p7t.jpg"
+]
 
-@app.on_message(filters.command("ping", prefixes=["/", "!"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_photo(
-        photo="https://files.catbox.moe/leaexg.jpg",
+        photo=random.choice(Vampire_PIC),
+        has_spoiler=True,
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await Shivi.ping()
@@ -37,29 +54,3 @@ async def ping_com(client, message: Message, _):
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(_),
     )
-
-
-@app.on_message(filters.command(["eco", "co"], prefixes=["/", "e", "E"]) & filters.reply & filters.user(list(SUDOERS)))
-async def eco_reply(client: Client, message):
-
-    if not message.reply_to_message:
-        await message.reply("**⋟ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜsᴇʀ's ᴍᴇssᴀɢᴇ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.**")
-        return
-      
-    command_parts = message.text.split(" ", 1)
-    if len(command_parts) < 2:
-        await message.reply("**⋟ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴇssᴀɢᴇ ᴀғᴛᴇʀ** `/eco` **ᴄᴏᴍᴍᴀɴᴅ.**")
-        return
-
-    reply_text = command_parts[1]
-
-    await message.delete()
-    await message.reply_to_message.reply(reply_text)
-
-# ===========================================================
-# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 😎
-# 
-# 🧑‍💻 Developer : t.me/TheSigmaCoder
-# 🔗 Source link : GitHub.com/Im-Notcoder/Shivi-V2
-# 📢 Telegram channel : t.me/Purvi_Bots
-# ===========================================================
