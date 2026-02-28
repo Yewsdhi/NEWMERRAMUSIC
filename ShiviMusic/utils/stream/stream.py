02451c10dfb73f758abdd00cdc386ce469ec3dee
Badnam465 <kritikumari8665@gmail.com>
2026-02-28 23:13:15 +0530

    Add has_spoiler parameter to send_photo method

--- ShiviMusic/utils/stream/stream.py
+++ ShiviMusic/utils/stream/stream.py
@@ -82,6 +82,7 @@ async def stream(
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
+                    has_spoiler=True,
                     caption=_["stream_1"].format(f"https://t.me/{app.username}?start=info_{vidid}", title[:23], duration_min, user_mention),
                     reply_markup=InlineKeyboardMarkup(button),
                 )
