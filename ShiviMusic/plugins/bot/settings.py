66bc4de9316a5ca3eb947d17fab22e5a79824c99
kirtibots <kritikumari8665@gmail.com>
2026-03-21 21:17:54 +0530

    Update settings.py

--- ShiviMusic/plugins/bot/settings.py
+++ ShiviMusic/plugins/bot/settings.py
@@ -113,7 +113,8 @@ async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
 
         return await CallbackQuery.edit_message_media(
             InputMediaPhoto(
-                media=random.choice(NEXI_VID), 
+                media=random.choice(NEXI_VID),
+                 has_spoiler=True,
                 caption=_["start_2"].format(
                     CallbackQuery.from_user.mention,
                     app.mention
