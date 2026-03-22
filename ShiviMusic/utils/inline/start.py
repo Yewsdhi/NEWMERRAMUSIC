3af324f1340e152dc8ffbf1f486bce701b54e395
kirtibots <kritikumari8665@gmail.com>
2026-03-22 21:09:15 +0530

    Update start.py

--- ShiviMusic/utils/inline/start.py
+++ ShiviMusic/utils/inline/start.py
@@ -1,23 +1,26 @@
+# ===========================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 🚀
+# 
+# This source code is under MIT License 📜
+# ❌ Unauthorized forking, importing, or using this code
+#    without giving proper credit will result in legal action ⚠️
+# 
+# 📩 DM for permission : @TheSigmaCoder
+# ===========================================================
+
 from pyrogram.types import InlineKeyboardButton
 
 import config
 from ShiviMusic import app
 
 
-
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.PRIMARY,
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(
-                text=_["S_B_2"], 
-                url=config.SUPPORT_CHAT,
-                style=ButtonStyle.PRIMARY,
-            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -29,30 +32,22 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.PRIMARY,
-            ),
+            )
         ],
         [
-            InlineKeyboardButton(
-                text=_["S_B_4"],
-                callback_data="settings_back_helper",
-                style=ButtonStyle.PRIMARY,
-            ),
+            InlineKeyboardButton(text=_["S_B_9"], callback_data="sbot_cb"),  
+            InlineKeyboardButton(text=_["S_B_13"], callback_data="abot_cb"),
         ],
         [
-            InlineKeyboardButton(
-                text=_["S_B_6"],
-                url=f"https://t.me/{config.OWNER_USERNAME}",
-                style=ButtonStyle.DANGER,
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=config.SUPPORT_CHANNEL,
-                style=ButtonStyle.SUCCESS,
-            ),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
     ]
     return buttons
+
+# ===========================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 😎
+# 
+# 🧑‍💻 Developer : t.me/TheSigmaCoder
+# 🔗 Source link : GitHub.com/Im-Notcoder/Shivi-V2
+# 📢 Telegram channel : t.me/Purvi_Bots
+# ===========================================================
