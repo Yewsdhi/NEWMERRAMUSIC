4cf762cc7b537b9ae31a4dd78bd53c442c73cf56
kirtibots <kritikumari8665@gmail.com>
2026-03-23 21:56:50 +0530

    Update repo_upload.py

--- ShiviMusic/plugins/tools/repo_upload.py
+++ ShiviMusic/plugins/tools/repo_upload.py
@@ -47,7 +47,7 @@ async def gitconfig(client, message):
     email = message.command[2]
     token = message.command[3]
     TEMP_CONFIG.update({"name": name, "email": email, "token": token, "timestamp": time.time()})
-    await message.reply("✅ **ɢɪᴛʜᴜʙ ᴄᴏɴꜰɪɢ sᴇᴛ sᴜᴄᴄᴇssғᴜʟʟʏ!** (ᴠᴀʟɪᴅ ꜰᴏʀ **5 ᴍɪɴᴜᴛᴇs**)")
+    await message.reply("✅ **ɢɪᴛʜᴜʙ ᴄᴏɴꜰɪɢ sᴇᴛ sᴜᴄᴄᴇssғᴜʟʟʏ!** (ᴠᴀʟɪᴅ ꜰᴏʀ **10 ᴍɪɴᴜᴛᴇs**)")
 
 
 @app.on_message(filters.command(["gitupload", "gt"]) & filters.user(OWNER_ID))
@@ -104,7 +104,7 @@ async def gitupload(client, message):
                 safe_rm(os.path.join(root, ".git"))
 
         user = g.get_user()
-        repo = user.create_repo(repo_name, private=is_private, description="🎉 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴜᴘʟᴏᴀᴅ ʙʏ :- ʙᴀᴅɴᴀᴍ ʙᴏᴛs 🌺", auto_init=False)
+        repo = user.create_repo(repo_name, private=is_private, description="🎉 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴜᴘʟᴏᴀᴅ ʙʏ :- ᴋɪʀᴛɪ ʙᴏᴛs 🌺", auto_init=False)
 
 
         run(["git", "init"], cwd=final_path)
@@ -116,9 +116,9 @@ async def gitupload(client, message):
 
         status_out = subprocess.run(["git", "status", "--porcelain"], cwd=final_path, text=True, capture_output=True)
         if status_out.stdout.strip():
-            run(["git", "commit", "-m", "ʙᴀᴅɴᴀᴍ ʙᴏᴛs !!"], cwd=final_path)
+            run(["git", "commit", "-m", "ᴋɪʀᴛɪ ʙᴏᴛs !!"], cwd=final_path)
         else:
-            run(["git", "commit", "--allow-empty", "-m", "ʙᴀᴅɴᴀᴍ ʙᴏᴛs !!"], cwd=final_path)
+            run(["git", "commit", "--allow-empty", "-m", "ᴋɪʀᴛɪ ʙᴏᴛs !!"], cwd=final_path)
 
         run(["git", "branch", "-M", branch_name], cwd=final_path)
         run(["git", "push", "-u", "origin", branch_name], cwd=final_path)
