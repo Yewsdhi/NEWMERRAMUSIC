e0c45a83c3594a7b93483d7346c39bb161f7d4e4
kirtibots <kritikumari8665@gmail.com>
2026-03-23 22:04:32 +0530

    Update queue.py

--- ShiviMusic/utils/inline/queue.py
+++ ShiviMusic/utils/inline/queue.py
@@ -71,7 +71,7 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="✙ ʌᴅᴅ ϻє ɪη ʏσυʀ ɢʀσυᴘ ✙", url=f"https://t.me/{app.username}?startgroup=true")
         ],
         [
-            InlineKeyboardButton(text="⌯ ᴍᴜsɪᴄ-ʙᴏᴛ ⌯", url="https://t.me/Kirtisongbot"),
+            InlineKeyboardButton(text="⌯ ᴍᴜsɪᴄ-ʙᴏᴛ ⌯", url="https://t.me/Kritiprobot"),
             InlineKeyboardButton(text="⌯ ᴀʟʟ-ʙᴏᴛs ⌯", url="https://t.me/Kirti_update"),
         ],
         [
