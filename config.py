b916e3a32efec07d6f915e5138dd93fb372fdb08
kirtibots <kritikumari8665@gmail.com>
2026-03-26 00:39:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,8 +32,10 @@ ASSUSERNAME = getenv("ASSUSERNAME")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOGGER_ID = int(getenv("LOGGER_ID", -1003670001038))
 
-BASE_URL = getenv("API_URL", 'https://BabyAPI.Pro') 
-API_KEY = getenv('API_KEY', 'ADMINBABYX20F56755E70E0694DDCC844F5F1BB465') 
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_GjLUhA7Xsu_5Dr_xBdFZLr8LzorcKIkK') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+
 
 # ======================================================
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
