8519f0a64e2b1f4c49cc7d368cc30dbac266bb7e
kirtibots <kritikumari8665@gmail.com>
2026-04-09 14:32:40 +0530

    Update button labels for seeking in play.py

--- ShiviMusic/utils/inline/play.py
+++ ShiviMusic/utils/inline/play.py
@@ -76,9 +76,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
          [
-             InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20"),
+             InlineKeyboardButton(text="⋟ - 𝟤𝟢 s", callback_data="seek_backward_20"),
              InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/III_Yadav_op_III?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙"),
-             InlineKeyboardButton(text="𝟤𝟢 s + >", callback_data="seek_forward_20")
+             InlineKeyboardButton(text="𝟤𝟢 s + ⋟", callback_data="seek_forward_20")
          ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
     ]
@@ -97,9 +97,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],
         [
-             InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20"),
+             InlineKeyboardButton(text="⪢ - 𝟤𝟢 s", callback_data="seek_backward_20"),
              InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/III_Yadav_op_III?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙"),
-             InlineKeyboardButton(text="𝟤𝟢 s+ >", callback_data="seek_forward_20")
+             InlineKeyboardButton(text="𝟤𝟢 s+ ⋟", callback_data="seek_forward_20")
          ],
             [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
 
