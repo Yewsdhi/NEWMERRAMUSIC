c61bfd64d72554892d238d0126ecffcd56ab19c8
kirtibots <kritikumari8665@gmail.com>
2026-04-11 11:24:40 +0530

    Update settings.py

--- ShiviMusic/plugins/bot/settings.py
+++ ShiviMusic/plugins/bot/settings.py
@@ -51,21 +51,21 @@ from config import BANNED_USERS, OWNER_ID
 
 
 NEXI_VID = [
-    "https://files.catbox.moe/x5lytj.jpg",
-    "https://files.catbox.moe/psya34.jpg",
-    "https://files.catbox.moe/leaexg.jpg",
-    "https://files.catbox.moe/b0e4vk.jpg",
-    "https://files.catbox.moe/1b1wap.jpg",
-    "https://files.catbox.moe/ommjjk.jpg",
-    "https://files.catbox.moe/onurxm.jpg",
-    "https://files.catbox.moe/97v75k.jpg",
-    "https://files.catbox.moe/t833zy.jpg",
-    "https://files.catbox.moe/472piq.jpg",
-    "https://files.catbox.moe/qwjeyk.jpg",
-    "https://files.catbox.moe/t0hopv.jpg",
-    "https://files.catbox.moe/u5ux0j.jpg",
-    "https://files.catbox.moe/h1yk4w.jpg",
-    "https://files.catbox.moe/gl5rg8.jpg",
+    "https://files.catbox.moe/4dag72.jpg",
+    "https://files.catbox.moe/8tscun.jpg",
+    "https://files.catbox.moe/bil9o3.jpg",
+    "https://files.catbox.moe/69iigg.jpg",
+    "https://files.catbox.moe/18368t.jpg",
+    "https://files.catbox.moe/wsesja.jpg",
+    "https://files.catbox.moe/4lnygg.jpg",
+    "https://files.catbox.moe/jz7q9v.jpg",
+    "https://files.catbox.moe/rc1nof.jpg",
+    "https://files.catbox.moe/0s7q4k.jpg",
+    "https://files.catbox.moe/z1x4ba.jpg",
+    "https://files.catbox.moe/187bms.jpg",
+    "https://files.catbox.moe/s5c7vp.jpg",
+    "https://files.catbox.moe/d4416d.jpg",
+    "https://files.catbox.moe/fy2lf1.jpg",
 ]
 
 
