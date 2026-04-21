4d86f425667ca4954ae2f90800ca1499393139d2
kirtibots <kritikumari8665@gmail.com>
2026-04-21 18:04:30 +0530

    Update button text for seeking functionality

--- ShiviMusic/utils/inline/play.py
+++ ShiviMusic/utils/inline/play.py
@@ -81,9 +81,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER),
         ],
          [
-             InlineKeyboardButton(text="⋟ - 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
+             InlineKeyboardButton(text="- 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
              InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/lll_Rony_xd_lll?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
-             InlineKeyboardButton(text="𝟤𝟢 s + ⋟", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
+             InlineKeyboardButton(text="𝟤𝟢 s + ", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
          ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
     ]
@@ -102,9 +102,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER),
          ],
         [
-             InlineKeyboardButton(text="⋟ - 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
+             InlineKeyboardButton(text="- 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
              InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/lll_Rony_xd_lll?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
-             InlineKeyboardButton(text="𝟤𝟢 s+ ⋟", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
+             InlineKeyboardButton(text="𝟤𝟢 s+ ", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
          ],
             [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
 
