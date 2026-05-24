d5ce398dc70712a4550126731cfee17a84acdf7b
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-05-24 14:03:13 +0530

    Replace button labels with new design

--- ShiviMusic/utils/stuffs/buttons.py
+++ ShiviMusic/utils/stuffs/buttons.py
@@ -16,25 +16,25 @@ import config
 class BUTTONS(object):
     ABUTTON = [
     [
-        InlineKeyboardButton("• sυᴘᴘσʀᴛ •", url="https://t.me/kirti_supprot_group", style=ButtonStyle.PRIMARY),
-        InlineKeyboardButton("• υᴘᴅᴧᴛєs •", url="https://t.me/Kirti_update", style=ButtonStyle.SUCCESS)
+        InlineKeyboardButton("⌯ sυᴘᴘσʀᴛ ⌯", url="https://t.me/kirti_supprot_group", style=ButtonStyle.PRIMARY),
+        InlineKeyboardButton("⌯ υᴘᴅᴧᴛєs ⌯", url="https://t.me/Kirti_update", style=ButtonStyle.SUCCESS)
     ],
     [
-        InlineKeyboardButton("• ❍ᴡηєʀ •", user_id=config.OWNER_ID, style=ButtonStyle.PRIMARY),
-        InlineKeyboardButton("• ʙᴧᴄᴋ •", callback_data="settingsback_helper", style=ButtonStyle.DANGER)
+        InlineKeyboardButton("⌯ ❍ᴡηєʀ ⌯", user_id=config.OWNER_ID, style=ButtonStyle.PRIMARY),
+        InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data="settingsback_helper", style=ButtonStyle.DANGER)
     ]
 ]
 
     INFO_BUTTON = [
     [
-        InlineKeyboardButton("• ʀєᴘσ •", callback_data="gib_source", style=ButtonStyle.PRIMARY),
+        InlineKeyboardButton("⌯ ʀєᴘσ ⌯", callback_data="gib_source", style=ButtonStyle.PRIMARY),
         InlineKeyboardButton("ʏᴛ-ᴀᴘɪ 🎟️", callback_data="bot_info_data", style=ButtonStyle.SUCCESS),
-        InlineKeyboardButton("• ʟᴧηɢᴜᴧɢє •", callback_data="LG", style=ButtonStyle.PRIMARY),
+        InlineKeyboardButton("⌯ ʟᴧηɢᴜᴧɢє ⌯", callback_data="LG", style=ButtonStyle.PRIMARY),
     ],
     [
         
-        InlineKeyboardButton("• sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ •", url="https://t.me/kirti_chat_support", style=ButtonStyle.PRIMARY),
-        InlineKeyboardButton("• ʙᴧᴄᴋ •", callback_data="settingsback_helper", style=ButtonStyle.DANGER),
+        InlineKeyboardButton("⌯ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ⌯", url="https://t.me/kirti_chat_support", style=ButtonStyle.PRIMARY),
+        InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data="settingsback_helper", style=ButtonStyle.DANGER),
     ]
     ]
     
@@ -42,7 +42,7 @@ class BUTTONS(object):
 
     INFO_NEW = [
     [
-        InlineKeyboardButton("• ʙᴧᴄᴋ •", callback_data="settings_back_helper", style=ButtonStyle.DANGER)
+        InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data="settings_back_helper", style=ButtonStyle.DANGER)
     ],
     ]
     
