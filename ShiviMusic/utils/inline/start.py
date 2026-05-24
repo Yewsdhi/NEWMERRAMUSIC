5bbecf6aba187c24070f18de0e3144872d24a5c4
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-05-24 05:57:35 +0530

    Update start.py

--- ShiviMusic/utils/inline/start.py
+++ ShiviMusic/utils/inline/start.py
@@ -9,6 +9,7 @@
 # ===========================================================
 
 from pyrogram.types import InlineKeyboardButton
+from pyrogram.enums import ButtonStyle
 
 import config
 from ShiviMusic import app
@@ -18,9 +19,12 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true",
+                style=ButtonStyle.PRIMARY,
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT,
+                style=ButtonStyle.SUCCESS,
+            ),
         ],
     ]
     return buttons
@@ -32,14 +36,21 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
+                style=ButtonStyle.PRIMARY,
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_9"], callback_data="sbot_cb"),  
-            InlineKeyboardButton(text=_["S_B_13"], callback_data="abot_cb"),
+            InlineKeyboardButton(text=_["S_B_9"], callback_data="sbot_cb",
+                style=ButtonStyle.SUCCESS,  
+            ),
+            InlineKeyboardButton(text=_["S_B_13"], callback_data="abot_cb",
+                style=ButtonStyle.PRIMARY,
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper",
+                style=ButtonStyle.DANGER,
+            ),
         ],
     ]
     return buttons
@@ -47,7 +58,7 @@ def private_panel(_):
 # ===========================================================
 # ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 😎
 # 
-# 🧑‍💻 Developer : t.me/TheSigmaCoder
-# 🔗 Source link : GitHub.com/Im-Notcoder/Shivi-V2
-# 📢 Telegram channel : t.me/Purvi_Bots
+# 🧑‍💻 Developer : t.me/badnam 
+# 🔗 Source link : GitHub.com/kirtibots/Shivi-V2
+# 📢 Telegram channel : t.me/kirti_Bots
 # ===========================================================
