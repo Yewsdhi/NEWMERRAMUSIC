220782d65f0fa5cad3f1404a8fbc1f81fe05fc6d
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-05-31 21:22:24 +0530

    Update speed.py

--- ShiviMusic/utils/inline/speed.py
+++ ShiviMusic/utils/inline/speed.py
@@ -9,7 +9,6 @@
 # ===========================================================
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums import ButtonStyle
 
 
 def speed_markup(_, chat_id):
@@ -19,38 +18,32 @@ def speed_markup(_, chat_id):
                 InlineKeyboardButton(
                     text="🕒 0.5x",
                     callback_data=f"SpeedUP {chat_id}|0.5",
-                    style=ButtonStyle.PRIMARY,
                 ),
                 InlineKeyboardButton(
                     text="🕓 0.75x",
                     callback_data=f"SpeedUP {chat_id}|0.75",
-                    style=ButtonStyle.SUCCESS,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["P_B_4"],
                     callback_data=f"SpeedUP {chat_id}|1.0",
-                    style=ButtonStyle.PRIMARY,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text="🕤 1.5x",
                     callback_data=f"SpeedUP {chat_id}|1.5",
-                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text="🕛 2.0x",
                     callback_data=f"SpeedUP {chat_id}|2.0",
-                    style=ButtonStyle.DANGER,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
-                    style=ButtonStyle.DANGER,
                 ),
             ],
         ]
