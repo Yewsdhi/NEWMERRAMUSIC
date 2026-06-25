9442c3a4cf09cfb7cf4ac04d559e276bab23cbaf
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-06-25 17:16:33 +0530

    Update support group links in repo.py

--- ShiviMusic/plugins/bot/repo.py
+++ ShiviMusic/plugins/bot/repo.py
@@ -10,7 +10,7 @@ start_txt = """**
  
 ❉  ʏᴏᴜ ᴄᴧη мʏ ᴜsє ᴘᴜʙʟɪᴄ ʀєᴘσs !!  
 
-✼ || [˹ᴋɪʀᴛɪ ꭙ ʙᴏᴛѕ˼ 💞](https://t.me/KRITI_SUPPORT_GROUP) ||
+✼ || [˹ᴋɪʀᴛɪ ꭙ ʙᴏᴛѕ˼ 💞](https://t.me/annu_support) ||
  
 ❊ ʀᴜη 24x7 ʟᴧɢ ϝʀєє ᴡɪᴛʜσᴜᴛ sᴛσᴘ**
 """
@@ -23,11 +23,11 @@ async def repo_command(_, msg):
             InlineKeyboardButton("✙ ᴧᴅᴅ ϻє вᴧʙʏ ✙", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-            InlineKeyboardButton("• ʜєʟᴘ •", url="https://t.me/KRITI_SUPPORT_GROUP"),
-            InlineKeyboardButton("• 𝛅ᴜᴘᴘσʀᴛ •", url="https://t.me/KRITI_SUPPORT_GROUP"),
+            InlineKeyboardButton("• ʜєʟᴘ •", url="https://t.me/annu_support"),
+            InlineKeyboardButton("• 𝛅ᴜᴘᴘσʀᴛ •", url="https://t.me/annu_support"),
         ],
         [
-            InlineKeyboardButton("• ϻᴧɪη ʙσᴛ •", url="https://t.me/Sanantinimusicbot"),
+            InlineKeyboardButton("• ϻᴧɪη ʙσᴛ •", url="https://t.me/Kirshnamusicbot?startgroup=true"),
         ],
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
