c33c764cbbe5107c1c592bbc0a869a283991ef33
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-12 14:11:03 +0530

    Update start.py

--- ShiviMusic/utils/inline/start.py
+++ ShiviMusic/utils/inline/start.py
@@ -23,7 +23,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                icon_custom_emoji_id="4958489311726011319"
+                icon_custom_emoji_id=4958489311726011319"
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
@@ -35,7 +35,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 url=f"https://t.me/{app.username}?start=help",
-                icon_custom_emoji_id="4956420911310832630"
+                icon_custom_emoji_id=4956420911310832630"
             ),
         ],
     ]
@@ -48,14 +48,14 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                icon_custom_emoji_id="4956475826762679249"
+                icon_custom_emoji_id=4956475826762679249"
             )
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_6"],
                 url=config.SUPPORT_CHANNEL,
-                icon_custom_emoji_id="4956475826762679249"
+                icon_custom_emoji_id=4956475826762679249"
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
@@ -74,7 +74,7 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{_clean_username(config.OWNER_USERNAME)}",
-                icon_custom_emoji_id="4956475826762679249"
+                icon_custom_emoji_id=4956475826762679249"
             ),
         ],
     ]
