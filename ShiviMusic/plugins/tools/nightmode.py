0ec1f8ff22ca5a38ee8da2fe54989b9497018f8c
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-13 08:33:41 +0530

    Fix syntax error in nightmode.py

--- ShiviMusic/plugins/tools/nightmode.py
+++ ShiviMusic/plugins/tools/nightmode.py
@@ -52,7 +52,7 @@ buttons = InlineKeyboardMarkup(
 NIGHT_MSG_BUTTONS = InlineKeyboardMarkup(
     [[
         InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/annu_support"), 
-        InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/annu_updates") 
+                    InlineKeyboardButton(text="✙ ʌᴅᴅ ϻє ɪη ʏσυʀ ɢʀσυᴘ ✙", url=f"https://t.me/{app.username}?startgroup=true"), 
     ]]
 )
 
