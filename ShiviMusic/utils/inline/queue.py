80767ce83d732787e48faf01e316902314b9bf09
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-07-16 09:02:17 +0530

    Update queue.py

--- ShiviMusic/utils/inline/queue.py
+++ ShiviMusic/utils/inline/queue.py
@@ -1,23 +1,7 @@
-# -----------------------------------------------
-# 🔸 VampireMusic Project
-# 🔹 Developed & Maintained by: Vampire Bots (https://github.com/TEAM-VAMPIRE-OP)
-# 📅 Copyright © 2025 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by TEAM-VAMPIRE-OP
-# -----------------------------------------------
-
-
-import config
 from typing import Union
-from config import OWNER_ID
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from ShiviMusic import app
+
 
 def queue_markup(
     _,
@@ -81,19 +65,6 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
-        [
-            InlineKeyboardButton("ʏᴛ-ᴀᴘɪ sᴛᴀᴛᴜs 💌", callback_data="bot_info_data"),
-        ],
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"
-            )
-        ],
+        [InlineKeyboardButton(text="𝐂ʟᴏsᴇ[✗]", callback_data="close")],
     ]
     return buttons
