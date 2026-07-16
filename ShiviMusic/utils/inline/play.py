23363c254089c95d7a4b4eb002c7f45f64d38c18
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-16 09:16:15 +0530

    Update import and callback data for ShiviMusic

--- ShiviMusic/utils/inline/play.py
+++ ShiviMusic/utils/inline/play.py
@@ -1,7 +1,7 @@
 import math
 from config import SUPPORT_CHAT, OWNER_USERNAME, SUPPORT_CHANNEL
 from pyrogram.types import InlineKeyboardButton, WebAppInfo
-from KanhaMusic import app
+from ShiviMusic import app
 import config
 from ShiviMusic.utils.formatters import time_to_seconds
 
@@ -112,11 +112,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"kanhaPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"Shivilaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"kanhaPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"Shivilaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
