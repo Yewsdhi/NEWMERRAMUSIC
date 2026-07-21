c8717476d1ca148a3a1a68e4c767c515d037e01f
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-21 09:07:52 +0530

    Fix formatting of InlineKeyboardButton callbacks

--- ShiviMusic/utils/inline/play.py
+++ ShiviMusic/utils/inline/play.py
@@ -65,7 +65,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {remaining}",
-                callback_data="GetTimer"
+                callback_data="GetTimer",
             )
         ],
         [
@@ -75,7 +75,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="⪻ -𝟸𝟶s", callback_data="seek_backward_20",)
+            InlineKeyboardButton(text="⪻ -𝟸𝟶s", callback_data="seek_backward_20"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="+𝟸𝟶s ⪼", callback_data="seek_forward_20"),
         ],
@@ -90,10 +90,10 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"), 
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"), 
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"), 
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
         [
             InlineKeyboardButton(text="⪻ -𝟸𝟶s", callback_data="seek_backward_20"),
