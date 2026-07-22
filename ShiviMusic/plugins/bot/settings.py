535ffefb890e27e7d3f43c806649702b1cc33153
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-22 14:42:35 +0530

    Add copyright and legal notice to settings.py
    Added copyright notice and legal disclaimer to settings.py

--- ShiviMusic/plugins/bot/settings.py
+++ ShiviMusic/plugins/bot/settings.py
@@ -1,3 +1,6 @@
+# ===========================================================
+# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 🚀
+# 
 # This source code is under MIT License 📜
 # ❌ Unauthorized forking, importing, or using this code
 #    without giving proper credit will result in legal action ⚠️
