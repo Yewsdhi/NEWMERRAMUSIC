3fb3b424abc2fea66bc9fe5a386c4d77a54ebbb3
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-22 13:48:54 +0530

    Update play.py

--- ShiviMusic/utils/inline/play.py
+++ ShiviMusic/utils/inline/play.py
@@ -1,7 +1,19 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
 import math
-from pyrogram.types import InlineKeyboardButton
-from ShiviMusic.utils.formatters import time_to_seconds
+from config import SUPPORT_CHAT, OWNER_USERNAME
+from pyrogram.types import InlineKeyboardButton, WebAppInfo
+from pyrogram.enums import ButtonStyle
 from ShiviMusic import app
+import config
+from ShiviMusic.utils.formatters import time_to_seconds
+
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
@@ -9,16 +21,19 @@ def track_markup(_, videoid, user_id, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_1"],
                 callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+                style=ButtonStyle.SUCCESS,
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+                style=ButtonStyle.PRIMARY,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
+                style=ButtonStyle.DANGER,
             )
         ],
     ]
@@ -28,61 +43,51 @@ def track_markup(_, videoid, user_id, channel, fplay):
 def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
-
-    remaining_sec = duration_sec - played_sec
-    if remaining_sec < 0:
-        remaining_sec = 0
-
-    rem_min = remaining_sec // 60
-    rem_sec = remaining_sec % 60
-    remaining = f"{rem_min:02d}:{rem_sec:02d}"
-
-    percentage = (played_sec / duration_sec) * 100 if duration_sec else 0
+    percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-
     if 0 < umm <= 10:
-        bar = "|⚪─────────|"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "|━⚪────────|"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "|━━⚪───────|"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "|━━━⚪──────|"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "|━━━━⚪─────|"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "|━━━━━⚪────|"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "|━━━━━━⚪───|"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "|━━━━━━━⚪──|"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "|━━━━━━━━⚪─|"
+        bar = "————————◉—"
     else:
-        bar = "|━━━━━━━━━⚪|"
-
+        bar = "—————————◉"
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {remaining}",
+                text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
+                style=ButtonStyle.PRIMARY,
             )
         ],
         [
-            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}", style=ButtonStyle.SUCCESS),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER),
         ],
-        [
-            InlineKeyboardButton(text="⪻ -𝟸𝟶s", callback_data="seek_backward_20"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="+𝟸𝟶s ⪼", callback_data="seek_forward_20"),
-        ],
-        [
-            InlineKeyboardButton(text="✙ 𝐀ᴅᴅ 𝐌є", url=f"https://t.me/{app.username}?startgroup=true"),
-            InlineKeyboardButton(text="𝐂ʟᴏsᴇ[✗]", callback_data="close"),
-        ],
+         [
+             InlineKeyboardButton(text="- 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
+             InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/lll_Rony_xd_lll?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
+             InlineKeyboardButton(text="𝟤𝟢 s + ", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
+         ],
+         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
+    ]
+        
     ]
     return buttons
 
@@ -90,20 +95,20 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-        ],
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}", style=ButtonStyle.SUCCESS),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER),
+         ],
         [
-            InlineKeyboardButton(text="⪻ -𝟸𝟶s", callback_data="seek_backward_20"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="+𝟸𝟶s ⪼", callback_data="seek_forward_20"),
-        ],
-        [
-            InlineKeyboardButton(text="✙ 𝐀ᴅᴅ 𝐌є", url=f"https://t.me/{app.username}?startgroup=true"),
-            InlineKeyboardButton(text="𝐂ʟᴏsᴇ[✗]", callback_data="close"),
-        ],
+             InlineKeyboardButton(text="- 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
+             InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/lll_Rony_xd_lll?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
+             InlineKeyboardButton(text="𝟤𝟢 s+ ", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
+         ],
+            [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
+
+        ]
     ]
     return buttons
 
@@ -114,20 +119,24 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_1"],
                 callback_data=f"ShiviPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                style=ButtonStyle.SUCCESS,
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
                 callback_data=f"ShiviPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                style=ButtonStyle.PRIMARY,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
+                style=ButtonStyle.DANGER,
             ),
         ],
     ]
     return buttons
+                
 
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
@@ -136,12 +145,14 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_3"],
                 callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+                style=ButtonStyle.SUCCESS,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
+                style=ButtonStyle.DANGER,
             ),
         ],
     ]
@@ -155,25 +166,38 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_1"],
                 callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+                style=ButtonStyle.SUCCESS,
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+                style=ButtonStyle.PRIMARY,
             ),
         ],
         [
             InlineKeyboardButton(
                 text="◁",
                 callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+                style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {query}|{user_id}",
+                style=ButtonStyle.DANGER,
             ),
             InlineKeyboardButton(
                 text="▷",
                 callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+                style=ButtonStyle.PRIMARY,
             ),
         ],
     ]
     return buttons
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
