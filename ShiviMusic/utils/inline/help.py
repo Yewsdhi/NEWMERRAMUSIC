c1e1e00cbcc834bd4610f44a290b5e7fc3326e78
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-22 14:53:50 +0530

    Update help.py

--- ShiviMusic/utils/inline/help.py
+++ ShiviMusic/utils/inline/help.py
@@ -3,178 +3,133 @@
 # 
 # This source code is under MIT License 📜
 # ❌ Unauthorized forking, importing, or using this code
-#    without giving proper credit will result in legal action ⚠️
+#    without giving proper credit will result in legal action ⚠️
 # 
 # 📩 DM for permission : @TheSigmaCoder
 # ===========================================================
 
 from typing import Union
-import random
 
-from pyrogram import filters, types
-from pyrogram.types import InlineKeyboardMarkup, Message, InputMediaPhoto
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import ButtonStyle
 
 from ShiviMusic import app
-from ShiviMusic.utils import help_pannel
-from ShiviMusic.utils.database import get_lang
-from ShiviMusic.utils.decorators.language import LanguageStart, languageCB
-from ShiviMusic.utils.inline.help import help_back_markup, private_help_panel
-from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
-from strings import get_string, helpers
-from ShiviMusic.utils.stuffs.buttons import BUTTONS
-from ShiviMusic.utils.stuffs.helper import Helper
 
 
-START_IMG = [
-    "https://files.catbox.moe/et1kky.jpg",
-    "https://files.catbox.moe/r6xs75.jpg",
-    "https://files.catbox.moe/qropc3.jpg",
-    "https://files.catbox.moe/nlbahf.jpg",
-    "https://files.catbox.moe/njrl6e.jpg",
-    "https://files.catbox.moe/7p0po1.jpg",
-    "https://files.catbox.moe/9sxqlx.jpg",
-    "https://files.catbox.moe/xrme38.jpg",
-    "https://files.catbox.moe/1nz3wk.jpg",
-    "https://files.catbox.moe/ev9586.jpg",
-    "https://files.catbox.moe/hjfr1n.jpg",
-    "https://files.catbox.moe/68c2m9.jpg",
-    "https://files.catbox.moe/1ol7pj.jpg",
-    "https://files.catbox.moe/v9hqvi.jpg",
-    "https://files.catbox.moe/v9hqvi.jpg",
-]
-
-@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
-@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
-async def helper_private(
-    client: app, update: Union[types.Message, types.CallbackQuery]
-):
-    is_callback = isinstance(update, types.CallbackQuery)
-    if is_callback:
-        try:
-            await update.answer()
-        except:
-            pass
-        chat_id = update.message.chat.id
-        language = await get_lang(chat_id)
-        _ = get_string(language)
-        keyboard = help_pannel(_, True)
-        await update.edit_message_text(
-            _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
-        )
-    else:
-        try:
-            await update.delete()
-        except:
-            pass
-        language = await get_lang(update.chat.id)
-        _ = get_string(language)
-        keyboard = help_pannel(_)
-        await update.reply_photo(
-            photo=START_IMG_URL,
-            has_spoiler=True,
-            caption=_["help_1"].format(SUPPORT_CHAT),
-            reply_markup=keyboard,
-        )
-
-
-@app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
-@LanguageStart
-async def help_com_group(client, message: Message, _):
-    keyboard = private_help_panel(_)
-    await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
-
-@app.on_callback_query(filters.regex("wel_cb") & ~BANNED_USERS)
-async def helper_cb(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(Helper.HELP_WEL, reply_markup=InlineKeyboardMarkup(BUTTONS.INFO_NEW))
-
-@app.on_callback_query(filters.regex("lock_cb") & ~BANNED_USERS)
-async def helper_cb(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(Helper.HELP_LOCK, reply_markup=InlineKeyboardMarkup(BUTTONS.INFO_NEW))
-
-@app.on_callback_query(filters.regex("night_cb") & ~BANNED_USERS)
-async def helper_cb(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(Helper.HELP_NIGHT, reply_markup=InlineKeyboardMarkup(BUTTONS.INFO_NEW))
-
-@app.on_callback_query(filters.regex("abot_cb") & ~BANNED_USERS)
-async def helper_cb(client, CallbackQuery):
-    bot = await client.get_me()
-    bot_mention = bot.mention
-
-    await CallbackQuery.edit_message_text(
-        Helper.HELP_ABOUT.format(bot_mention),
-        reply_markup=InlineKeyboardMarkup(BUTTONS.INFO_BUTTON),
-    )
-
-@app.on_callback_query(filters.regex("sbot_cb") & ~BANNED_USERS)
-async def helper_cb(client, CallbackQuery):
-    bot = await client.get_me()
-    bot_mention = bot.mention
-
-    await CallbackQuery.edit_message_text(
-        Helper.HELP_SUPPORT.format(bot_mention),
-        reply_markup=InlineKeyboardMarkup(BUTTONS.ABUTTON),
-    )
-
-@app.on_callback_query(filters.regex("ibot_cb") & ~BANNED_USERS)
-async def helper_cb(client, CallbackQuery):
-    bot = await client.get_me()
-    bot_mention = bot.mention
-
-    await CallbackQuery.edit_message_text(
-        Helper.HELP_INFO.format(bot_mention),
-        reply_markup=InlineKeyboardMarkup(BUTTONS.INFO_BUTTON),
-    )
-
-@app.on_callback_query(filters.regex("back_cb") & ~BANNED_USERS)
-async def back_cb(client, CallbackQuery):
-    photo = random.choice(START_IMG)
-    bot = await client.get_me()
-    bot_mention = bot.mention
-
-    await CallbackQuery.edit_message_media(
-        media=InputMediaPhoto(
-            media=photo,
-            caption=Helper.HELP_ABOUT.format(bot_mention)
+def help_pannel(_, START: Union[bool, int] = None):
+    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER)]
+    second = [
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data="settingsback_helper",
+            style=ButtonStyle.DANGER,
         ),
-        reply_markup=InlineKeyboardMarkup(BUTTONS.INFO_BUTTON)
-    )
+    ]
 
-@app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
-@languageCB
-async def helper_cb(client, CallbackQuery, _):
-    callback_data = CallbackQuery.data.strip()
-    cb = callback_data.split(None, 1)[1]
-    keyboard = help_back_markup(_)
-    if cb == "hb1":
-        await CallbackQuery.edit_message_text(helpers.HELP_1, reply_markup=keyboard)
-    elif cb == "hb2":
-        await CallbackQuery.edit_message_text(helpers.HELP_2, reply_markup=keyboard)
-    elif cb == "hb3":
-        await CallbackQuery.edit_message_text(helpers.HELP_3, reply_markup=keyboard)
-    elif cb == "hb4":
-        await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
-    elif cb == "hb5":
-        await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
-    elif cb == "hb6":
-        await CallbackQuery.edit_message_text(helpers.HELP_6, reply_markup=keyboard)
-    elif cb == "hb7":
-        await CallbackQuery.edit_message_text(helpers.HELP_7, reply_markup=keyboard)
-    elif cb == "hb8":
-        await CallbackQuery.edit_message_text(helpers.HELP_8, reply_markup=keyboard)
-    elif cb == "hb9":
-        await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
-    elif cb == "hb10":
-        await CallbackQuery.edit_message_text(helpers.HELP_10, reply_markup=keyboard)
-    elif cb == "hb11":
-        await CallbackQuery.edit_message_text(helpers.HELP_11, reply_markup=keyboard)
-    elif cb == "hb12":
-        await CallbackQuery.edit_message_text(helpers.HELP_12, reply_markup=keyboard)
-    elif cb == "hb13":
-        await CallbackQuery.edit_message_text(helpers.HELP_13, reply_markup=keyboard)
-    elif cb == "hb14":
-        await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
-    elif cb == "hb15":
-        await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
+    mark = second if START else first
+
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_25"],
+                    callback_data="help_callback hb1",
+                    style=ButtonStyle.DANGER,
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_26"],
+                    callback_data="help_callback hb2",
+                    style=ButtonStyle.DANGER,
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_28"],
+                    callback_data="help_callback hb3",
+                    style=ButtonStyle.DANGER,
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_27"],
+                    callback_data="help_callback hb4",
+                    style=ButtonStyle.SUCCESS,
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_31"],
+                    callback_data="help_callback hb5",
+                    style=ButtonStyle.SUCCESS,
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_29"],
+                    callback_data="help_callback hb6",
+                    style=ButtonStyle.SUCCESS,
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_33"],
+                    callback_data="help_callback hb7",
+                    style=ButtonStyle.PRIMARY,
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_30"],
+                    callback_data="help_callback hb8",
+                    style=ButtonStyle.PRIMARY,
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_32"],
+                    callback_data="help_callback hb9",
+                    style=ButtonStyle.PRIMARY,
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="• ᴡᴇʟᴄᴏᴍᴇ •",
+                    callback_data="wel_cb",
+                    style=ButtonStyle.SUCCESS,
+                ),
+                InlineKeyboardButton(
+                    text="• ʟᴏᴄᴋs •",
+                    callback_data="lock_cb",
+                    style=ButtonStyle.SUCCESS,
+                ),
+                InlineKeyboardButton(
+                    text="• ɴɪɢʜᴛᴍᴏᴅᴇ •",
+                    callback_data="night_cb",
+                    style=ButtonStyle.SUCCESS,
+                ),
+            ],
+            mark,
+        ]
+    )
+    return upl
+
+
+def help_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"settings_back_helper",
+                    style=ButtonStyle.DANGER,
+                ),
+            ]
+        ]
+    )
+    return upl
+
+
+def private_help_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_4"],
+                url=f"https://t.me/{app.username}?start=help",
+                style=ButtonStyle.PRIMARY,
+            ),
+        ],
+    ]
+    return buttons
 
 # ===========================================================
 # ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 😎
