91a5d74e5267b14e39cdb4ba2bf9124914483ed9
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-08 13:18:55 +0530

    Remove unused import from database.py
    Removed unused import from database.py

--- ShiviMusic/utils/database.py
+++ ShiviMusic/utils/database.py
@@ -13,7 +13,6 @@ from typing import Dict, List, Union
 
 from ShiviMusic import userbot
 from ShiviMusic.core.mongo import mongodb
-from .guest import *
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
