2ff17b51625e1237d20a4d3b379d72f3dd322905
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-08-09 00:05:12 +0530

    Update logger.py

--- ShiviMusic/utils/logger.py
+++ ShiviMusic/utils/logger.py
@@ -1,128 +1,33 @@
-from pyrogram.enums import ParseMode, ChatMemberStatus
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import ParseMode
 
 from ShiviMusic import app
 from ShiviMusic.utils.database import is_on_off
 from config import LOGGER_ID
 
 
-async def play_logs(message, streamtype, thumbnail=None):
-
+async def play_logs(message, streamtype):
     if await is_on_off(2):
-
-        owner_id = None
-        owner_username = None
-        group_link = None
-
-        # Group Link
-        try:
-            if message.chat.username:
-                group_link = f"https://t.me/{message.chat.username}"
-            else:
-                group_link = await app.export_chat_invite_link(
-                    message.chat.id
-                )
-        except Exception:
-            pass
-
-
-        # Owner Details
-        try:
-            async for admin in app.get_chat_members(
-                message.chat.id,
-                filter="administrators"
-            ):
-                if admin.status == ChatMemberStatus.OWNER:
-                    owner_id = admin.user.id
-                    owner_username = admin.user.username
-                    break
-        except Exception:
-            pass
-
-
         logger_text = f"""
 <b>❖ {app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
 <b>● ᴄʜᴀᴛ ɪᴅ ➠</b> <code>{message.chat.id}</code>
 <b>● ᴄʜᴀᴛ ɴᴀᴍᴇ ➠</b> {message.chat.title}
-
-<b>● ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.chat.username if message.chat.username else "Private"}
-
-<b>● ᴏᴡɴᴇʀ ɪᴅ ➠</b> <code>{owner_id if owner_id else "Unknown"}</code>
-<b>● ᴏᴡɴᴇʀ ➠</b> @{owner_username if owner_username else "No Username"}
+<b>● ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.chat.username}
 
 <b>● ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>
 <b>● ɴᴀᴍᴇ ➠</b> {message.from_user.mention}
+<b>● ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}
 
-<b>● ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username if message.from_user.username else "None"}
-
-<b>● sᴛʀᴇᴀᴍᴛʏᴘᴇ ➠</b> {streamtype}
-"""
-
-
-        buttons = []
-
-        # Group Button
-        if group_link:
-            buttons.append(
-                InlineKeyboardButton(
-                    "🔗 ᴄʜᴀᴛ ʟɪɴᴋ",
-                    url=group_link
-                )
-            )
-
-
-        # Owner Button
-        if owner_id:
-
-            if owner_username:
-                buttons.append(
-                    InlineKeyboardButton(
-                        "👑 Owner",
-                        url=f"https://t.me/{owner_username}"
-                    )
-                )
-
-            else:
-                buttons.append(
-                    InlineKeyboardButton(
-                        "👑 Owner",
-                        url=f"tg://user?id={owner_id}"
-                    )
-                )
-
-
-        markup = None
-
-        if buttons:
-            markup = InlineKeyboardMarkup(
-                [buttons]
-            )
-
-
+<b>● ǫᴜᴇʀʏ ➠</b> {message.text.split(None, 1)[1]}
+<b>● sᴛʀᴇᴀᴍᴛʏᴘᴇ ➠</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
-
             try:
-                if thumbnail:
-
-                    await app.send_photo(
-                        chat_id=LOGGER_ID,
-                        photo=thumbnail,
-                        caption=logger_text,
-                        parse_mode=ParseMode.HTML,
-                        disable_notification=True,
-                        reply_markup=markup,
-                    )
-
-                else:
-
-                    await app.send_message(
-                        chat_id=LOGGER_ID,
-                        text=logger_text,
-                        parse_mode=ParseMode.HTML,
-                        disable_notification=True,
-                        reply_markup=markup,
-                    )
-
-            except Exception as e:
-                print(f"LOGGER ERROR : {e}")
+                await app.send_message(
+                    chat_id=LOGGER_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return
