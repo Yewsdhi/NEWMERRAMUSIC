8bc211ae946baf96b9672df63f7c5624f26583ad
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-08-14 15:51:12 +0530

    Add files via upload

--- ShiviMusic/utils/inline/help.py
+++ ShiviMusic/utils/inline/help.py
@@ -83,8 +83,8 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                 InlineKeyboardButton(
-                    text="• ᴀᴜᴛᴏᴘʟᴀʏ •",
-                    callback_data="autoplay_cb",
+                    text="• ᴡᴇʟᴄᴏᴍᴇ •",
+                    callback_data="wel_cb",
                     style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
